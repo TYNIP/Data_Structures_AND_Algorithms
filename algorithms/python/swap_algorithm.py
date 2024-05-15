ea68fb9b68e77c419b3fee39a65f1c88050a34ce
TYNIP <130399006+TYNIP@users.noreply.github.com>
2024-05-14 23:54:30 -0600

    python, queue, double link and swap algorithm

---
+++ algorithms/python/swap_algorithm.py
@@ -0,0 +1,106 @@
+import Node 
+import LinkedList
+
+""" BASIC """
+def swap_nodes(input_list, val1, val2):
+  print(f'Swapping {val1} with {val2}')
+
+  node1_prev = None
+  node2_prev = None
+  node1 = input_list.head_node
+  node2 = input_list.head_node
+
+  if val1 == val2:
+    print("Elements are the same - no swap needed")
+    return
+
+  while node1 is not None:
+    if node1.get_value() == val1:
+      break
+    node1_prev = node1
+    node1 = node1.get_next_node()
+
+  while node2 is not None:
+    if node2.get_value() == val2:
+      break
+    node2_prev = node2
+    node2 = node2.get_next_node()
+
+  if (node1 is None or node2 is None):
+    print("Swap not possible - one or more element is not in the list")
+    return
+
+  if node1_prev is None:
+    input_list.head_node = node2
+  else:
+    node1_prev.set_next_node(node2)
+
+  if node2_prev is None:
+    input_list.head_node = node1
+  else:
+    node2_prev.set_next_node(node1)
+
+  temp = node1.get_next_node()
+  node1.set_next_node(node2.get_next_node())
+  node2.set_next_node(temp)
+
+
+ll = LinkedList.LinkedList()
+for i in range(10):
+  ll.insert_beginning(i)
+
+print(ll.stringify_list())
+swap_nodes(ll, 9, 5)
+print(ll.stringify_list())
+
+""" TECHNIQUES """
+#pt1 - last node
+def nth_last_node(linked_list, n):
+  current = None
+  tail_seeker = linked_list.head_node
+  count = 1
+  while tail_seeker:
+    tail_seeker = tail_seeker.get_next_node()
+    count += 1
+    if count >= n + 1:
+      if current is None:
+        current = linked_list.head_node
+      else:
+        current = current.get_next_node()
+  return current
+
+def generate_test_linked_list():
+  linked_list = LinkedList()
+  for i in range(50, 0, -1):
+    linked_list.insert_beginning(i)
+  return linked_list
+
+# Test
+test_list = generate_test_linked_list()
+print(test_list.stringify_list())
+nth_last = nth_last_node(test_list, 4)
+print(nth_last.value)
+
+#pt2 - find middle node
+def find_middle(linked_list):
+  fast = linked_list.head_node
+  slow = linked_list.head_node
+  while fast:
+    fast = fast.get_next_node()
+    if fast:
+      fast = fast.get_next_node()
+      slow = slow.get_next_node()
+  return slow
+
+
+def generate_test_linked_list(length):
+  linked_list = LinkedList()
+  for i in range(length, 0, -1):
+    linked_list.insert_beginning(i)
+  return linked_list
+
+# TEST
+test_list = generate_test_linked_list(7)
+print(test_list.stringify_list())
+middle_node = find_middle(test_list)
+print(middle_node.value)
